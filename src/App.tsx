cc45a3398915e92950dff0a30dc8228a18863eca
Michael-Barboza <michaelalexanderbarboza@outlook.com>
2025-07-24 18:56:58 -0300

    agregamos LocalStorage y reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Form from "./components/Form";
-import { useReducer } from "react";
+import { useReducer, useEffect, useMemo } from "react";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
 import ActivityList from "./components/ActivityList";
 
 
 const App = () => {
   const [state, dispatch] = useReducer(activityReducer, initialState);
+  useEffect(()=>{
+localStorage.setItem('activities', JSON.stringify(state.activities))
+  },[state.activities])
+
+  const canRestartApp = useMemo(() => state.activities.length, [state.activities]);
   return (
     <>
      <header className="bg-orange-800 text-white p-4">
@@ -13,6 +18,13 @@ const App = () => {
     <h1 className="text-lg font-bold uppercase">
       Agenda de Actividades
     </h1>
+    <button
+      disabled={!canRestartApp}
+      onClick={() => dispatch({ type: "restart-app" })}
+      className="ml-4 bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 disabled:opacity-50"
+    >
+      Reiniciar
+    </button>
   </div>
 </header>
 
